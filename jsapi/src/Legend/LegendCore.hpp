9544ccf4e9b87c5fe04d626d44934b2b32e032a6
Guzhuooo <guzhuooo@users.noreply.github.com>
2026-08-28 14:57:33 +0800

    hpp: restore private method declarations

--- jsapi/src/Legend/LegendCore.hpp
+++ jsapi/src/Legend/LegendCore.hpp
@@ -141,6 +141,10 @@ private:
     void genCave(uint32_t seed);
     void genTemple(uint32_t seed, int pillars);
     void spawnMonsters(uint32_t seed, int count, const std::vector<MobSpawn> &pool, int minDist);
+    Mob *findMob(int id);
+    bool moveTo(Mob &m, float tx, float ty, float dist, float dt);
+    Mob *pickAttackTarget(float range);
+    void attackMob(Mob &m, float mult, float now, AttackResult &out);
 };
 
 } // namespace legend
